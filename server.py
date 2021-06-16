fb53b9b5fe5f407c9ece221c6139bfe0cb916a87
Rosa Valentine <rosalammora@gmail.com>
2021-06-16 22:31:34 +0700

    Fixed message broadcasting

--- server.py
+++ server.py
@@ -1,61 +1,106 @@
-from socket import AF_INET, socket, SOCK_STREAM
+import socket
 from threading import Thread
+import sys
+from collections import defaultdict as df
+import time
 
+TARGET_HOST = "127.0.0.3"
+TARGET_PORT = 33000
 
-def accept_incoming_connections():
-    """Sets up handling for incoming clients."""
-    while True:
-        client, client_address = SERVER.accept()
-        print("%s:%s has connected.\n" % client_address)
-        client.send(bytes("Greetings from the cave! Now type your name and press enter!\n", "utf8"))
-        addresses[client] = client_address
-        Thread(target=handle_client, args=(client,)).start()
+class Server:
+    def __init__(self):
+        self.rooms = df(list)
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server.bind((TARGET_HOST, int(TARGET_PORT)))
+        self.server.listen(100)
+        ACCEPT_THREAD = Thread(target=self.acceptIncomingConnections)
+        ACCEPT_THREAD.start()
+        ACCEPT_THREAD.join()
 
+    def acceptIncomingConnections(self):
+        while True:
+            connection, address = self.server.accept()
+            print(str(address[0]) + ":" + str(address[1]) + " Terhubung")
+            Thread(target=self.handleClients, args=(connection,)).start()
+            # start_new_thread(self.handleClients(connection))
 
-def handle_client(client):  # Takes client socket as argument.
-    """Handles a single client connection."""
+    def handleClients(self, connection):
+        user_id = connection.recv(1024).decode().replace("User ", "")
+        room_id = connection.recv(1024).decode().replace("Join ", "")
 
-    name = client.recv(BUFSIZ).decode("utf8")
-    welcome = 'Welcome %s! If you ever want to quit, type {quit} to exit.\n' % name
-    client.send(bytes(welcome, "utf8"))
-    msg = "%s has joined the chat!\n" % name
-    broadcast(bytes(msg, "utf8"))
-    clients[client] = name
-
-    while True:
-        msg = client.recv(BUFSIZ)
-        if msg != bytes("{quit}", "utf8"):
-            broadcast(msg, name + ": ")
+        if room_id not in self.rooms:
+            connection.send("Grup Baru Terbentuk".encode())
         else:
-            client.send(bytes("{quit}", "utf8"))
-            client.close()
-            del clients[client]
-            broadcast(bytes("%s has left the chat.\n" % name, "utf8"))
-            break
+            connection.send("Selamat Datang di Grup".encode())
 
+        self.rooms[room_id].append(connection)
 
-def broadcast(msg, prefix=""):  # prefix is for name identification.
-    """Broadcasts a message to all the clients."""
+        while True:
+            try:
+                message = connection.recv(1024)
+                print(str(message.decode()))
 
-    for sock in clients:
-        sock.send(bytes(prefix, "utf8") + msg)
+                if message:
+                    if str(message.decode()) == "FILE":
+                        self.broadcastFile(connection, room_id, user_id)
 
+                    else:
+                        message_to_send = str(user_id) + " | " + message.decode()
+                        self.broadcastMessage(connection, room_id, message_to_send)
 
-clients = {}
-addresses = {}
+                else:
+                    self.remove(connection, room_id)
 
-HOST = '127.0.0.1'
-PORT = 33000
-BUFSIZ = 1024
-ADDR = (HOST, PORT)
+            except Exception as e:
+                print(repr(e))
+                print("Client disconnected")
+                break
 
-SERVER = socket(AF_INET, SOCK_STREAM)
-SERVER.bind(ADDR)
+    def broadcastFile(self, connection, room_id, user_id):
+        fileName = connection.recv(1024).decode()
+        fileLength = connection.recv(1024).decode()
+        for client in self.rooms[room_id]:
+            if client !=connection:
+                try:
+                    client.send("FILE".encode())
+                    time.sleep(0.1)
+                    client.send(fileName.encode())
+                    time.sleep(0.1)
+                    client.send(fileLength.encode())
+                    time.sleep(0.1)
+                    client.send(user_id.encode())
+                except:
+                    client.close()
+                    self.remove(client, room_id)
+
+        total = 0
+        print(fileName, fileLength)
+        while str(total) != fileLength:
+            data = connection.recv(1024)
+            total = total +len(data)
+            for client in self.rooms[room_id]:
+                if client !=  connection:
+                    try:
+                        client.send(data)
+                    except:
+                        client.close()
+                        self.remove(client, room_id)
+        print("Terkirim")
+
+    # Send text messages
+    def broadcastMessage(self, connection, room_id, message):
+        for client in self.rooms[room_id]:
+            if client != connection:
+                try:
+                    client.send(message.encode())
+                except:
+                    client.close()
+                    self.remove(client, room_id)
+
+    def remove(self, connection, room_id):
+        if connection in self.rooms[room_id]:
+            self.rooms[room_id].remove(connection)
 
 if __name__ == "__main__":
-    SERVER.listen(5)
-    print("Waiting for connection...")
-    ACCEPT_THREAD = Thread(target=accept_incoming_connections)
-    ACCEPT_THREAD.start()
-    ACCEPT_THREAD.join()
-    SERVER.close()
+    server = Server()
