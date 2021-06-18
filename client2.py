02071716e71b1e6bfd43077e7b955f98ef0ef4ab
SalsabilaH12 <harlensalsabila@gmail.com>
2021-06-18 15:26:51 +0700

    update gui

--- client2.py
+++ client2.py
@@ -20,28 +20,28 @@ class GUI:
 
         self.login = tk.Toplevel()
 
-        self.login.title("Login SChat")
+        self.login.title("Login ChatProgjar")
         self.login.resizable(width=False, height=False)
-        self.login.configure(width=400, height=400)
+        self.login.configure(width=400, height=400, bg="#45818e")
 
-        self.pls = tk.Label(self.login, text="Silakan Login ke Chatroom", justify=tk.CENTER, font="Helvetica 12 bold")
+        self.pls = tk.Label(self.login, text="Silakan Login ke Chatroom", justify=tk.CENTER, font="Helvetica 12 bold", bg="#45818e")
 
         self.pls.place(relheight=0.15, relx=0.2, rely=0.07)
 
-        self.userLabelName = tk.Label(self.login, text="Username ", font="Helvetica 11")
+        self.userLabelName = tk.Label(self.login, text="Username ", font="Helvetica 11", bg="#45818e")
         self.userLabelName.place(relheight=0.2, relx=0.1, rely=0.25)
 
         self.userEntryName = tk.Entry(self.login, font="Helvetica 11")
         self.userEntryName.place(relwidth=0.4, relheight=0.1, relx=0.35, rely=0.3)
         self.userEntryName.focus()
 
-        self.roomLabelName = tk.Label(self.login, text="Chatroom ID ", font="Helvetica 11")
+        self.roomLabelName = tk.Label(self.login, text="Chatroom ID ", font="Helvetica 11", bg="#45818e")
         self.roomLabelName.place(relheight=0.2, relx=0.1, rely=0.4)
 
         self.roomEntryName = tk.Entry(self.login, font="Helvetica 11", show="*")
         self.roomEntryName.place(relwidth=0.4, relheight=0.1, relx=0.35, rely=0.45)
 
-        self.go = tk.Button(self.login, text="LOGIN", font="Helvetica 12 bold",
+        self.go = tk.Button(self.login, text="LOGIN", font="Helvetica 12 bold", bg="#1c3439", fg="#fff",
                             command=lambda: self.authenticate(self.userEntryName.get(), self.roomEntryName.get()))
         self.go.place(relx=0.35, rely=0.62)
 
@@ -63,9 +63,17 @@ class GUI:
         self.Window.deiconify()
         self.Window.title("Chatroom")
         self.Window.resizable(width=False, height=False)
-        self.Window.configure(width=400, height=600, bg="#ebebeb")
+        self.Window.configure(width=400, height=600, bg="#45818e")
+        self.chatBoxHead = tk.Label(self.Window,
+                                    bg="#45818e",
+                                    fg="#EAECEE",
+                                    text=self.name,
+                                    font="Helvetica 11 bold",
+                                    pady=5)
 
-        self.textCons = tk.Text(self.Window, width=20, height=2, bg="#17202A", fg="#EAECEE", padx=5, pady=5,
+        self.chatBoxHead.place(relwidth=1)
+
+        self.textCons = tk.Text(self.Window, width=20, height=2, bg="#dae6e8", fg="#000", padx=5, pady=5,
                                 font="Helvetica 11")
         self.textCons.place(relheight=0.745, relwidth=1, rely=0.08)
         self.textCons.config(cursor="arrow")
@@ -74,30 +82,30 @@ class GUI:
         scrollbar.config(command=self.textCons.yview)
         self.textCons.config(state=tk.DISABLED)
 
-        self.labelBottom = tk.Label(self.Window, bg="#ABB2B9", height=80)
+        self.labelBottom = tk.Label(self.Window, bg="#45818e", height=80)
         self.labelBottom.place(relwidth=1, rely=0.8)
 
-        self.messageBox = tk.Entry(self.labelBottom, bg="#2C3E50", fg="#EAECEE", font="Helvetica 11")
+        self.messageBox = tk.Entry(self.labelBottom, bg="#dae6e8", fg="#000", font="Helvetica 11")
         self.messageBox.place(relwidth=0.74, relheight=0.03, rely=0.008, relx=0.011)
         self.messageBox.focus()
 
-        self.textSendButton = tk.Button(self.labelBottom, text="Kirim", width=10, bg="#ABB2B9",
-                                        font="Helvetica 10 bold",
+        self.textSendButton = tk.Button(self.labelBottom, text="Kirim", width=10, bg="#1c3439",
+                                        font="Helvetica 10 bold", fg="#fff",
                                         command=lambda: self.sendTextButton(self.messageBox.get()))
         self.textSendButton.place(relx=0.77, rely=0.008, relheight=0.03, relwidth=0.22)
 
-        self.labelFile = tk.Label(self.Window, bg="#ABB2B9", height=70)
+        self.labelFile = tk.Label(self.Window, bg="#45818e", height=70)
         self.labelFile.place(relwidth=1, rely=0.9)
 
         self.fileLocation = tk.Label(self.labelFile, text="Upload file", bg="#2C3E50", fg="#EAECEE",
                                      font="Helvetica 11")
         self.fileLocation.place(relwidth=0.65, relheight=0.03, rely=0.008, relx=0.011)
 
-        self.fileBrowseButton = tk.Button(self.labelFile, text="Cari", width=10, bg="#ABB2B9", font="Helvetica 10 bold",
+        self.fileBrowseButton = tk.Button(self.labelFile, text="Cari", width=10, bg="#1c3439", font="Helvetica 10", fg="#fff",
                                           command=self.browseFile)
         self.fileBrowseButton.place(relx=0.67, rely=0.008, relheight=0.03, relwidth=0.15)
 
-        self.fileSendButton = tk.Button(self.labelFile, text="Kirim File", width=10, bg="#ABB2B9",
+        self.fileSendButton = tk.Button(self.labelFile, text="Kirim File", width=10,font="Helvetica 10", bg="#1c3439",fg="#fff",
                                         command=self.sendFile)
         self.fileSendButton.place(relx=0.84, rely=0.008, relheight=0.03, relwidth=0.15)
 
@@ -187,3 +195,4 @@ class GUI:
 
 if __name__ == "__main__":
     gui = GUI()
+
